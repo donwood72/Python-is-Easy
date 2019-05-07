67613bd5972dd2516fdf6f5c2244550144ba2126
Don Wood <50380795+donwood72@users.noreply.github.com>
2019-05-07 21:57:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+This is a simple Python program.
+In this, I have used an album example, some
+of which is fictitious to add a little spice to
+those who actually KNOW the artist, so I'm having a little
+fun with those folks.
+"""
+
+Artist = "Queen"	#create a string for the artist name
+Genre = "Pop"		#create a string type for the Genre
+Album = "Best Of"	#another string type for an album name
+Song = "Bohemian Rhapsody" #and another for the Song name, basically all names are strings
+Year = 2001		#Type of Int for whole numbers
+Duration = 98	#Type of Int for whole numbers
+Rating = 4.5	#Type of Float for fractions
+Artwork = "Dark Angels" #Type of string for names
+
+print("Artist: " + Artist)	
+print("Genre: " + Genre)
+print("Album: " + Album)
+print("Song: " + Song)
+print("Year: " + str(Year))
+print("Duration:" + str(Duration))
+print("Rating: " + str(Rating))
+print("Artwork: " + Artwork)
+
+
